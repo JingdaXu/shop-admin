239253ea6dfd1b67874c7c2d8f6b4118663e48d6
v_jingdaxu <v_jingdaxu@tencent.com>
2023-02-15 17:08:15 +0800

    'update'

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.vue']
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
