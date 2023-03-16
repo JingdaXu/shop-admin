8129251e83666ba27076a591f900b3b7dd0bd0f7
v_jingdaxu <v_jingdaxu@tencent.com>
2023-03-16 18:21:24 +0800

    'feat:增加toggleSidebar'

---
+++ src/layout/components/AppHeader/ToggleSidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-icon>
+    <el-icon
+      :size="size"
+      :color="color"
+    >
+      <Expand
+        @click="comStore.changeCollapsed"
+        v-if="comStore.isCollapsed"
+      />
+      <Fold
+        @click="comStore.changeCollapsed"
+        v-else
+      />
+    </el-icon>
+  </el-icon>
+</template>
+
+<script setup lang='ts'>
+import { commonStore } from '@/store/index'
+import { ref } from 'vue'
+const comStore = commonStore()
+const size = ref('20')
+const color = ref('black')
+</script>
+
+<style lang="scss" scoped>
+i{
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+    cursor: pointer;
+}
+</style>
